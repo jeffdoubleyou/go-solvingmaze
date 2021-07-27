1622922c13f83a16ce5dda5f8c2113573b9c21bd
Jeffrey Weitz <jeffdoubleyou@gmail.com>
2021-07-27 15:15:34 -0400

    Fix models

--- models/service.go
+++ models/service.go
@@ -1,23 +1,17 @@
 package models
 
 type Service struct {
-	Status struct {
-		Success  bool     `json:"success"`
-		Messages []string `json:"messages"`
+	Carrier    string     `json:"carrier"`
+	Service    string     `json:"service"`
+	Currency   string     `json:"currency"`
+	Weight     float64    `json:"weight"`
+	WeightUnit string     `json:"weightUnit"`
+	Rate       float64    `json:"rate"`
+	Value      int        `json:"value"`
+	Packing    []*Package `json:"packing"`
+	Status     struct {
+		Success bool `json:"success"`
 	} `json:"status"`
-	Services []struct {
-		Carrier    string     `json:"carrier"`
-		Service    string     `json:"service"`
-		Currency   string     `json:"currency"`
-		Weight     float64    `json:"weight"`
-		WeightUnit string     `json:"weightUnit"`
-		Rate       float64    `json:"rate"`
-		Value      int        `json:"value"`
-		Packing    []*Package `json:"packing"`
-		Status     struct {
-			Success bool `json:"success"`
-		} `json:"status"`
-		ListRate float64   `json:"listRate"`
-		Delivery *Delivery `json:"delivery,omitempty"`
-	} `json:"services"`
+	ListRate float64   `json:"listRate"`
+	Delivery *Delivery `json:"delivery,omitempty"`
 }
